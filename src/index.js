3867013f8f94e2eeac825c86d62840e0c58dddec
wusiquan <1781554387@qq.com>
2019-02-09 00:40:57 +0800

    add autoCenter game config

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ let config = {
   backgroundColor: 0x000000,
   scale: {
     mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH,
     parent: 'match-three',
     width: gameConfig.width,
     height: gameConfig.height
